3d3f7c4432db5ffb302599711e3b31c54d8470a2
Schetinina <liudmila.schetinina@gmail.com>
2021-03-15 22:34:59 +0300

    feat: implement reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    return parseInt(reverseString(Math.abs(n).toString()), 10);
 }
+
+function reverseString (s){
+    let reversed = "";
+    for (let i = s.length - 1; i >= 0; i--) {
+        reversed = reversed + s[i];
+    }
+    return reversed; 
+}
+
